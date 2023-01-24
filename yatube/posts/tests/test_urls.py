45da8e9ffd8769b42c25241215d1933a463a3c08
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-24 16:05:27 +0300

    11111

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from posts.models import Group, Post
+from posts.models import Group, Post, User
 
 User = get_user_model()
 
