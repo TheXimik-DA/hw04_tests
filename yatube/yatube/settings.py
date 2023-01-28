31de94d6cefb04a483a66483a7613a40a5b1d490
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-01-28 11:07:53 +0300

    1111

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,8 +30,7 @@ INSTALLED_APPS = [
     'sorl.thumbnail',
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -110,3 +109,7 @@ LOGIN_REDIRECT_URL = 'posts:index'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
